ff782fa4af236b50741e2b8765df2fd69ce85d04
Nancy <nwu928@aucklanduni.ac.nz>
2024-07-31 18:25:17 +1200

    refactor navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,24 @@ import Link from "next/link"
 import Image from "next/image";
 import fridge from "../../public/fridge.svg"
 
-const Navbar = () => {
+export default function Navbar() {
+
+  const navList = [
+    {
+      link: "/",
+      text: "Kitchen",
+      icon: fridge.src
+    },
+    // {
+    //   link: "/",
+    //   text: "Cookbook"
+    // },
+    // {
+    //   link: "/",
+    //   text: "Calendar"
+    // },
+  ]
+
   return (
     <nav className="sticky z-[100] h-24 w-full px-8 md:px-24 flex items-center">
       <div className="grow">
@@ -12,18 +29,16 @@ const Navbar = () => {
           <span className="text-primary">dine</span><span className="text-secondary">in</span><span className="text-primary">.</span>
         </Link>
       </div>
-      <div>
-        <Link href="/" className="flex z-40 text-xl">
-          <span><Image src={fridge.src} alt="fridge-icon" width={32} height={32} /></span><span className="text-white pl-2">Kitchen</span>
-        </Link>
-      </div>
-      <div className="ml-28">
-        <Link href="/">
+      <div className="flex flex-row items-center gap-12">
+        {navList.map((navItem: any, key: number) => (
+          <Link href="/" className="flex z-40 text-xl" key={key}>
+            <span><Image src={navItem.icon} alt={`${navItem.text}-icon`} width={32} height={32} /></span><span className="text-white pl-2">{navItem.text}</span>
+          </Link>
+      ))}
+        <Link href="/" className="ml-16">
           <div className="avatar rounded-full min-h-12 min-w-12 bg-gray-100"></div>
         </Link>
       </div>
     </nav>
   )
 }
-
-export default Navbar
